083f7f214e509012ba15ec17dbf172f9ed46b206
Mihail Bubnov <mihail.bubnov@mediaopt.de>
2017-05-03 16:31:12 +0300

    GetTax service migrated

--- src/Service/GetTax.php
+++ src/Service/GetTax.php
@@ -2,7 +2,9 @@
 
 namespace Shopware\Plugins\MoptAvalara\Service;
 
-use Shopware\Plugins\MoptAvalara\Model\DocumentType;
+use Avalara\TransactionBuilder;
+use Avalara\TransactionAddressType;
+use Avalara\DocumentType;
 use Shopware\Plugins\MoptAvalara\Model\GetTaxRequest;
 
 /**
@@ -11,16 +13,78 @@ use Shopware\Plugins\MoptAvalara\Model\GetTaxRequest;
  */
 class GetTax extends AbstractService
 {
-
-    protected $SERVICE_PATH = '/1.0/tax/get';
-    
-    public function call(GetTaxRequest $getTaxRequest)
+    /**
+     * 
+     * @param GetTaxRequest $taxRequest
+     * @return \stdClass
+     */
+    public function calculate(GetTaxRequest $taxRequest)
     {
-        $response = $this->getSdkMain()->getClient()->post($this->getServiceUrl(), array(
-            'auth' => $this->getAuth(),
-            'body' => json_encode($getTaxRequest->toArray()),
-        ));
+        $client = $this->getAdapter()->getClient();
+        /* @var $originAddress \Shopware\Plugins\MoptAvalara\Model\Address */
+        $originAddress = $this->getAdapter()->getFactory('Address')->buildOriginAddress();
+        /* @var $address \Shopware\Plugins\MoptAvalara\Model\Address */
+        $address = $taxRequest->getAddresses()[0];
+        $tb = new TransactionBuilder($client, null, $taxRequest->getDocType(), $taxRequest->getCustomerCode());
+        $tb
+            ->withAddress(
+                TransactionAddressType::C_SHIPFROM, 
+                $originAddress->line1, 
+                null, 
+                null, 
+                $originAddress->city, 
+                $originAddress->region,
+                $originAddress->postalCode, 
+                $originAddress->country
+            )
+            ->withAddress(
+                TransactionAddressType::C_SHIPTO, 
+                $address->line1, 
+                null, 
+                null, 
+                $address->city, 
+                $address->region,
+                $address->postalCode, 
+                $address->country
+            )
+        ;
         
-        return $response->json();
+        foreach($taxRequest->getLines() as $line) {
+            /* @var $line \Shopware\Plugins\MoptAvalara\Model\Line */
+            $tb
+                ->withLine($line->getAmount(), $line->getQty(), $line->getTaxCode(), $line->getItemCode())
+                ->withLineParameter('itemCode', $line->getItemCode())
+            ;
+        }
+        
+        $response = $tb->create();
+        $this->fixItemCodes($taxRequest, $response);
+
+        return $response;
+    }
+    
+    /**
+     * There is a bug in Avalara API.
+     * If we use type SalesOrder and provide itemCode - we got empty itemCode in response.
+     * We are hoping that Avalara whould return lines in the same order.
+     * 
+     * @param GetTaxRequest $taxRequest
+     * @param \stdClass $taxInformation
+     */
+    private function fixItemCodes(GetTaxRequest $taxRequest, \stdClass $taxInformation)
+    {
+        if (DocumentType::C_SALESINVOICE === $taxRequest->getDocType()) {
+            return;
+        }
+        
+        foreach($taxRequest->getLines() as $i => $line) {
+            $lineNo = $i+1;
+            /* @var $line \Shopware\Plugins\MoptAvalara\Model\Line */
+            foreach ($taxInformation->lines as $taxLineInformation) {
+                if (empty($taxLineInformation->itemCode) && $lineNo == $taxLineInformation->lineNumber) {
+                    $taxLineInformation->itemCode = $line->getItemCode();
+                }
+            }
+        }
     }
 }
