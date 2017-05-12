7dc8312dcc6bbf4e41f0ad2bb4ec0abdb9158f31
Mihail Bubnov <mihail.bubnov@mediaopt.de>
2017-05-12 17:05:07 +0300

    Add LandedCostRequestParams with constants

---
+++ src/LandedCost/LandedCostRequestParams.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Shopware\Plugins\MoptAvalara\LandedCost;
+
+/**
+ * Config names holder for the Landed cost features
+ */
+class LandedCostRequestParams
+{
+    const LANDED_COST_INCOTERMS = 'AvaTax.LandedCost.Incoterms';
+    const LANDED_COST_SHIPPING_MODE = 'AvaTax.LandedCost.ShippingMode';
+    const LANDED_COST_HTSCODE = 'AvaTax.LandedCost.HTSCode';
+}
